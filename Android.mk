6b796f9a7400604350a0a943f578f1682ac5582d
sndnvaps <sndnvaps@gmail.com>
2016-03-18 21:59:18 +0800

    Fix build Error on MoKee mkm branch
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- Android.mk
+++ Android.mk
@@ -24,34 +24,34 @@ LOCAL_C_INCLUDES              := $(LOCAL_PATH)/include \
                                  $(LOCAL_PATH)/kexec/arch/arm/include \
                                  $(LOCAL_PATH)/kexec/libfdt \
                                  external/zlib
-LOCAL_SRC_FILES               := kexec/kexec.c kexec/ifdown.c \
-                                 kexec/kexec-elf.c kexec/kexec-elf-exec.c \
-                                 kexec/kexec-elf-core.c \
-                                 kexec/kexec-elf-rel.c \
-                                 kexec/kexec-elf-boot.c \
-                                 kexec/kexec-iomem.c \
-                                 kexec/firmware_memmap.c \
-                                 kexec/crashdump.c kexec/crashdump-xen.c \
-                                 kexec/phys_arch.c kexec/lzma.c \
-                                 kexec/zlib.c kexec/proc_iomem.c \
-                                 kexec/virt_to_phys.c \
-                                 kexec/arch/arm/phys_to_virt.c \
-                                 kexec/add_segment.c kexec/add_buffer.c \
-                                 kexec/arch_reuse_initrd.c \
-                                 kexec/arch_init.c \
-                                 kexec/arch/arm/kexec-elf-rel-arm.c \
-                                 kexec/arch/arm/kexec-zImage-arm.c \
-                                 kexec/arch/arm/kexec-uImage-arm.c \
-                                 kexec/arch/arm/kexec-arm.c \
-                                 kexec/arch/arm/mach.c \
-                                 kexec/arch/arm/mach-hammerhead.c \
-                                 kexec/arch/arm/mach-m8.c \
-                                 kexec/arch/arm/mach-shamu.c \
-                                 kexec/arch/arm/crashdump-arm.c \
-                                 kexec/kexec-uImage.c kexec/purgatory.c \
-                                 kexec/libfdt/fdt.c kexec/libfdt/fdt_ro.c \
-                                 kexec/libfdt/fdt_rw.c kexec/libfdt/fdt_strerror.c \
-                                 kexec/libfdt/fdt_sw.c kexec/libfdt/fdt_wip.c
+LOCAL_SRC_FILES	:= 	kexec/kexec.c kexec/ifdown.c \
+			kexec/kexec-elf.c kexec/kexec-elf-exec.c \
+			kexec/kexec-elf-core.c \
+			kexec/kexec-elf-rel.c \
+			kexec/kexec-elf-boot.c \
+			kexec/kexec-iomem.c \
+			kexec/firmware_memmap.c \
+			kexec/crashdump.c kexec/crashdump-xen.c \
+			kexec/phys_arch.c kexec/lzma.c \
+			kexec/zlib.c kexec/proc_iomem.c \
+			kexec/virt_to_phys.c \
+			kexec/arch/arm/phys_to_virt.c \
+			kexec/add_segment.c kexec/add_buffer.c \
+			kexec/arch_reuse_initrd.c \
+			kexec/arch_init.c \
+			kexec/arch/arm/kexec-elf-rel-arm.c \
+			kexec/arch/arm/kexec-zImage-arm.c \
+			kexec/arch/arm/kexec-uImage-arm.c \
+			kexec/arch/arm/kexec-arm.c \
+			kexec/arch/arm/mach.c \
+			kexec/arch/arm/mach-hammerhead.c \
+			kexec/arch/arm/mach-m8.c \
+			kexec/arch/arm/mach-shamu.c \
+			kexec/arch/arm/crashdump-arm.c \
+			kexec/kexec-uImage.c kexec/purgatory.c \
+			kexec/libfdt/fdt.c kexec/libfdt/fdt_ro.c \
+			kexec/libfdt/fdt_rw.c kexec/libfdt/fdt_strerror.c \
+			kexec/libfdt/fdt_sw.c kexec/libfdt/fdt_wip.c \
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES        := mrom_libutil_kt libz libc
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
